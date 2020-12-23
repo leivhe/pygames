8059a2592da515b7321375b21005b144058586bc
leivhe <leiv.hellebo@unit.no>
2020-12-23 13:30:29 +0100

    new global variable gameover, so one can start over

--- mastermind.py
+++ mastermind.py
@@ -1,7 +1,5 @@
 from random import randint
 
-import pygame
-
 WIDTH = 800
 HEIGHT = 770
 
@@ -31,6 +29,7 @@ riktigFargePaFeilPlass = 'riktig farge på feil plass'
 
 ferdigKnapp = Rect((500, 400), (100, 50))
 
+StartPåNyttKnapp = Rect((500, 100), (200,100))
 
 def tilfeldigFarge():
     return alleFarger[randint(0, 7)]
@@ -40,12 +39,12 @@ hemmeligKode = [tilfeldigFarge(), tilfeldigFarge(),
     tilfeldigFarge(), tilfeldigFarge()]
 
 gjetninger = []
-
+gameover = False
 
 
 
 def draw():
-    #print ("draw!")
+    global gameover
     screen.draw.filled_rect(gjett1[rect], gjett1[farge][farge])
     screen.draw.filled_rect(gjett2[rect], gjett2[farge][farge])
     screen.draw.filled_rect(gjett3[rect], gjett3[farge][farge])
@@ -57,19 +56,30 @@ def nesteFarge(farge):
     return alleFarger[(alleFarger.index(farge) + 1) % len(alleFarger)]
 
 def on_mouse_down(pos):
-    #print("on_mouse_down")
+    global gameover
     for gjett in alleGjett:
         if gjett[rect].collidepoint(pos):
             gjett[farge] = nesteFarge(gjett[farge])
-            #print(f"bytter farge til {gjett[farge]}")
 
     if ferdigKnapp.collidepoint(pos):
         #print(f"ferdig med gjetning: {len(gjetninger)}")
         gjetninger.append([gjett1[farge],gjett2[farge],gjett3[farge],gjett4[farge]])
         #print(gjetninger[len(gjetninger) - 1])
         sjekkKodeOgGiTilbakemelding()
-        flyttGjetninger()
+        print(f"on_mouse_down {gameover}")
+        if gameover:
+            visBareStartIgjenKnapp()
+        else:
+            flyttGjetninger()
 
+    if StartPåNyttKnapp.collidepoint(pos):
+        initState()
+
+def visBareStartIgjenKnapp():
+    for gjett in alleGjett:
+        gjett[rect].x += 1000
+    screen.draw.filled_rect(ferdigKnapp, (0,0,0))
+    ferdigKnapp.x += 1000
 
 
 def flyttGjetninger():
@@ -78,11 +88,12 @@ def flyttGjetninger():
 
 
 def sjekkKodeOgGiTilbakemelding():
+    global gameover
     gjett = gjetninger[len(gjetninger) - 1]
     kodekopi = hemmeligKode.copy()
     antallRiktigFargePaRiktigPlass = 0
     antallRiktigFargePaFeilPlass = 0
-    #print ("skal sjekke " + str(gjett) + ", opp mot " + str(hemmeligKode))
+    # print ("skal sjekke " + str(gjett) + ", opp mot " + str(hemmeligKode))
     for n in range(len(gjett)):
         if gjett[n] == kodekopi[n]:
             #print("riktig farge på riktig plass for " + str(n))
@@ -103,3 +114,25 @@ def sjekkKodeOgGiTilbakemelding():
             antallRiktigFargePaFeilPlass += 1
 
     screen.draw.text(f"{antallRiktigFargePaRiktigPlass} RR, {antallRiktigFargePaFeilPlass} RF", (gjett4[rect].x + 100, gjett4[rect].y), color="orange")
+    if antallRiktigFargePaRiktigPlass == 4:
+        screen.draw.textbox("Du klarte det! Start på nytt her", ferdigKnapp, color="orange")
+        gameover = True
+    if len(gjetninger) == 10:
+        screen.draw.textbox("Du klarte det desverre ikke. Prøv på nytt her", StartPåNyttKnapp, color="orange")
+        gameover = True
+
+def initState():
+    global gameover
+    print("initState")
+    screen.clear()
+    hemmeligKode = tilfeldigFarge(), tilfeldigFarge(), tilfeldigFarge(), tilfeldigFarge()
+    gjetninger.clear()
+    gameover = False
+    gjett1[rect].x = 50
+    gjett2[rect].x = 110
+    gjett3[rect].x = 170
+    gjett4[rect].x = 230
+    for gjett in alleGjett:
+        gjett[rect].y = 700
+    ferdigKnapp.x = 500
+    ferdigKnapp.y = 400
